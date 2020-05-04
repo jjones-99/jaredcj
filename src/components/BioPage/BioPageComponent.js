358a5531a34f1d703d432ab7e81b6e38f8a629cf
jjones-99 <jjones-99@live.com>
2020-05-04 14:28:05 -0600

    Updated Resume link.

--- src/components/BioPage/BioPageComponent.js
+++ src/components/BioPage/BioPageComponent.js
@@ -3,73 +3,92 @@ import JCJButton from "../custom-blocks/JCJButton";
 import NavbarComponent from "../custom-blocks/NavbarComponent";
 import EducationComponent from "./EducationComponent";
 import ExperienceComponent from "./ExperienceComponent";
-import {Col, Row} from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import "./BioPageComponent.scss";
 import ExposureComponent from "./ExposureComponent";
-import ResumePDF from "../../assets/Resume.pdf";
+import Resume from "../../assets/Resume050420.pdf";
 
-const BioPageComponent = () =>
-    <div className={""}>
-        <NavbarComponent page={"bio"}/>
+const BioPageComponent = () => (
+  <div className={""}>
+    <NavbarComponent page={"bio"} />
 
-        <article id={"jcj-resume"}>
-            <Row className={"jcj-resume-row"}>
-                <Col md className={"jcj-resume-col jcj-resume-header"}>
-                    <h2 className={"jcj-resume-title"}>RESUME</h2>
-                    <JCJButton link={ResumePDF} new_tab text={"View my resume. (104 KB)"}/>
-                </Col>
-                <Col md className={"jcj-resume-col"}>
-                    <section className={"jcj-resume-section"}>
-                        <h3>ABOUT ME</h3>
-                        <p>
-                            I want to be able to see and interact with the work that I produce. For that reason, I'm
-                            most interested in front-end web and app development, followed closely by full-stack. I
-                            want to help teams convey their brand and personality through their online presence,
-                            while providing an enjoyable experience for the consumer.
-                        </p>
-                        <p>
-                            I’m a hard-worker and do my best to enjoy every moment of life. It's not good to worry
-                            overmuch
-                            about all of the little things that went wrong when we can appreciate everything that went
-                            right.
-                            I’m always looking to share aid and positivity with others, and I love being able to do that
-                            through
-                            the work I do.
-                        </p>
-                        <p>
-                            I believe that the ability to learn and adapt is more important than
-                            knowing everything right off the bat. I thrive in environments where I can put extra work in
-                            to
-                            learn and improve the quality of the work I do.
-                        </p>
-                    </section>
-                </Col>
-            </Row>
+    <article id={"jcj-resume"}>
+      <Row className={"jcj-resume-row"}>
+        <Col md className={"jcj-resume-col jcj-resume-header"}>
+          <h2 className={"jcj-resume-title"}>RESUME</h2>
+          <JCJButton link={Resume} new_tab text={"View my resume. (104 KB)"} />
+        </Col>
+        <Col md className={"jcj-resume-col"}>
+          <section className={"jcj-resume-section"}>
+            <h3>ABOUT ME</h3>
+            <p>
+              I want to be able to see and interact with the work that I
+              produce. For that reason, I'm most interested in front-end web and
+              app development, followed closely by full-stack. I want to help
+              teams convey their brand and personality through their online
+              presence, while providing an enjoyable experience for the
+              consumer.
+            </p>
+            <p>
+              I’m a hard-worker and do my best to enjoy every moment of life.
+              It's not good to worry overmuch about all of the little things
+              that went wrong when we can appreciate everything that went right.
+              I’m always looking to share aid and positivity with others, and I
+              love being able to do that through the work I do.
+            </p>
+            <p>
+              I believe that the ability to learn and adapt is more important
+              than knowing everything right off the bat. I thrive in
+              environments where I can put extra work in to learn and improve
+              the quality of the work I do.
+            </p>
+          </section>
+        </Col>
+      </Row>
 
-            <Row className={"jcj-resume-row"}>
-                <Col md><ExperienceComponent/></Col>
-                <Col md className={"jcj-resume-col order-first order-md-last"}>
-                    <img src={require("../../assets/Splat.png")} alt=""
-                         className={"mr-md-4 jcj-resume-image"} height={"500px"}/>
-                </Col>
-            </Row>
+      <Row className={"jcj-resume-row"}>
+        <Col md>
+          <ExperienceComponent />
+        </Col>
+        <Col md className={"jcj-resume-col order-first order-md-last"}>
+          <img
+            src={require("../../assets/Splat.png")}
+            alt=""
+            className={"mr-md-4 jcj-resume-image"}
+            height={"500px"}
+          />
+        </Col>
+      </Row>
 
-            <Row className={"jcj-resume-row"}>
-                <Col md className={"jcj-resume-col"}>
-                    <img src={require("../../assets/Stretch.png")} alt=""
-                         className={"ml-md-4 jcj-resume-image"} height={"350px"}/>
-                </Col>
-                <Col md className={"jcj-resume-col"}><EducationComponent/></Col>
-            </Row>
+      <Row className={"jcj-resume-row"}>
+        <Col md className={"jcj-resume-col"}>
+          <img
+            src={require("../../assets/Stretch.png")}
+            alt=""
+            className={"ml-md-4 jcj-resume-image"}
+            height={"350px"}
+          />
+        </Col>
+        <Col md className={"jcj-resume-col"}>
+          <EducationComponent />
+        </Col>
+      </Row>
 
-            <Row className={"jcj-resume-row"}>
-                <Col md><ExposureComponent/></Col>
-                <Col md className={"jcj-resume-col order-first order-md-last"}>
-                    <img src={require("../../assets/Avocado.png")} alt=""
-                         className={"mr-md-4 jcj-resume-image"} height={"300px"}/>
-                </Col>
-            </Row>
-        </article>
-    </div>;
+      <Row className={"jcj-resume-row"}>
+        <Col md>
+          <ExposureComponent />
+        </Col>
+        <Col md className={"jcj-resume-col order-first order-md-last"}>
+          <img
+            src={require("../../assets/Avocado.png")}
+            alt=""
+            className={"mr-md-4 jcj-resume-image"}
+            height={"300px"}
+          />
+        </Col>
+      </Row>
+    </article>
+  </div>
+);
 
 export default BioPageComponent;
--- src/components/custom-blocks/JCJButton.js
+++ src/components/custom-blocks/JCJButton.js
@@ -1,29 +1,39 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faChevronDown, faChevronLeft, faChevronRight, faChevronUp} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  faChevronUp,
+} from "@fortawesome/free-solid-svg-icons";
 import "./JCJButton.scss";
 
 const getIconForDirection = (direction) => {
-    switch (direction) {
-        case "up":
-            return <FontAwesomeIcon icon={faChevronUp}/>;
-        case "down":
-            return <FontAwesomeIcon icon={faChevronDown}/>;
-        case "left":
-            return <FontAwesomeIcon icon={faChevronLeft}/>;
-        case "right":
-            return <FontAwesomeIcon icon={faChevronRight}/>;
-        default:
-            return;
-    }
+  switch (direction) {
+    case "up":
+      return <FontAwesomeIcon icon={faChevronUp} />;
+    case "down":
+      return <FontAwesomeIcon icon={faChevronDown} />;
+    case "left":
+      return <FontAwesomeIcon icon={faChevronLeft} />;
+    case "right":
+      return <FontAwesomeIcon icon={faChevronRight} />;
+    default:
+      return;
+  }
 };
 
-const JCJButton = ({link = "#", new_tab, text, direction = "none"}) =>
-    <a href={link} className={"jcj-button d-flex justify-content-between align-items-center"}
-       target={new_tab ? "_blank" : "_self"} rel={"noopener noreferrer"}>
-        <span>{getIconForDirection(direction)}</span>
-        <span className={"jcj-button-text"}>{text}</span>
-        <span>{getIconForDirection(direction)}</span>
-    </a>;
+const JCJButton = ({ link = "#", new_tab, text, direction = "none" }) => (
+  <a
+    href={link}
+    className={"jcj-button d-flex justify-content-between align-items-center"}
+    target={new_tab ? "_blank" : "_self"}
+    rel={"noopener noreferrer"}
+  >
+    <span>{getIconForDirection(direction)}</span>
+    <span className={"jcj-button-text"}>{text}</span>
+    <span>{getIconForDirection(direction)}</span>
+  </a>
+);
 
 export default JCJButton;
