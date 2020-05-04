82aa8d4e72c76173dbecfb7522c22c32878dd84d
jjones-99 <jjones-99@live.com>
2020-05-04 14:24:50 -0600

    Updated exposure list.

--- src/components/BioPage/ExposureComponent.js
+++ src/components/BioPage/ExposureComponent.js
@@ -1,31 +1,36 @@
 import React from "react";
 
-const ExposureComponent = () =>
-    <section className={"jcj-resume-section"}>
-        <h3>MY EXPOSURE</h3>
-        <section>
-            <p>
-                Through the coursework and projects that I've completed, I've gotten a lot of exposure to
-                quite a few technologies. Most importantly, I learned the most about these topics independently
-                in order to improve the quality of my work.
-            </p>
-            <ul className={"jcj-resume-exposure-list"}>
-                <li>Java</li>
-                <li>C</li>
-                <li>C++</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React.js</li>
-                <li>PHP</li>
-                <li>WordPress</li>
-                <li>Squarespace</li>
-                <li>Python</li>
-                <li>Angular.js</li>
-                <li>MongoDB</li>
-                <li>Node.js</li>
-            </ul>
-        </section>
-    </section>;
+const ExposureComponent = () => (
+  <section className={"jcj-resume-section"}>
+    <h3>MY EXPOSURE</h3>
+    <section>
+      <p>
+        Through the coursework and projects that I've completed, I've gotten a
+        lot of exposure to quite a few technologies and skills. Most
+        importantly, I learned the most about these topics independently in
+        order to improve the quality of my work.
+      </p>
+      <ul className={"jcj-resume-exposure-list"}>
+        <li>GitHub</li>
+        <li>Pair Programming</li>
+        <li>React</li>
+        <li>Angular</li>
+        <li>Node.js</li>
+        <li>MySQL</li>
+        <li>MongoDB</li>
+        <li>WordPress</li>
+
+        <li>JavaScript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Java</li>
+        <li>C++</li>
+        <li>C</li>
+        <li>PHP</li>
+        <li>Python</li>
+      </ul>
+    </section>
+  </section>
+);
 
 export default ExposureComponent;
